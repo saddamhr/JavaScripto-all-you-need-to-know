3b34724f1ca4c769d813f3024ee6577f44773b66
saddamhr <saddambubt65@gmail.com>
2020-04-05 23:52:28 +0600

    reserved words and naming convention in JS

---
+++ chapter-two/reserved-words-naming-convention.js
@@ -0,0 +1,8 @@
+// RESERVED WORDS IN JS
+// reserved words: http://www.javascripter.net/faq/reserved.htm
+// We do not use reserved words as variable name
+
+// VARIABLE NAMING CONVENTION
+// CAMEL CASE
+var mathNumber = 10
+var accountNumberDetailsId = 104

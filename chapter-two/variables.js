169d1c75710c0d65fb0e6a8e5dd938d9547bd420
saddamhr <saddambubt65@gmail.com>
2020-04-05 17:59:43 +0600

    variable is JS

---
+++ chapter-two/variables.js
@@ -0,0 +1,7 @@
+var name = 'Saddam Hossain'
+var age = 25
+
+console.log(name + ' knows JavaScript')
+console.log('His age is only '+ age)
+console.log(name + ' learning JavaScript')
+console.log('But his age is only ' +age +'!')

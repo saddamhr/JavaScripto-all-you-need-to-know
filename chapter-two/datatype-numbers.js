00761f7239e29a71c5a9e16e3106b35af5fa40c7
saddamhr <saddambubt65@gmail.com>
2020-04-06 00:20:23 +0600

    data type numbers

---
+++ chapter-two/datatype-numbers.js
@@ -0,0 +1,12 @@
+var n = 1451
+var f = 3.14
+var nn = Number('45.45')
+console.log(nn)
+console.log(Number.parseInt(nn))
+console.log(Number.parseFloat(nn))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+// Infinity
+console.log(1 / 0)
+// NaN
+console.log('abc' * 10);

18b9d8c11670413de55a2be0a98e648de77ef7a6
saddamhr <saddambubt65@gmail.com>
2020-04-06 01:10:19 +0600

    string

---
+++ chapter-two/string.js
@@ -0,0 +1,11 @@
+// String: Anything wrapped between quote is called string.
+
+var str = 'String'
+var str2 = "String"
+var str3 = `String`
+
+var str4 = String('Saddam Hossain')
+var str5 = String(156)
+var str6 = String(3.1416)
+
+console.log(str4, '\n'+str5, '\n'+str6+'\n')

ae214abee0f233c3ad5f63291a4ad40ee4375504
saddamhr <saddambubt65@gmail.com>
2020-04-06 13:00:46 +0600

    else in JS

---
+++ 3-chapter-three [handle-conditions-and-logic]/2-else.js
@@ -0,0 +1,16 @@
+var a = 10
+var b = 20
+
+if (a > b) {
+    console.log('A is greater than B')
+} else {
+    console.log('B is greater than A')
+}
+
+var n = 50
+
+if( n % 2 === 0) {
+    console.log(n +' is Even Number')
+} else {
+    console.log(n +' is Odd Number')
+}
---
+++ 3-chapter-three [handle-conditions-and-logic]/3-else-if.js
@@ -0,0 +1,20 @@
+var a = 20
+var b = 20
+
+if (a > b) {
+    console.log('A is greater than B')
+} else if( a < b) {
+    console.log('B is greater than A')
+} else {
+    console.log('They both are same')
+}
+
+var n = 1
+
+if( n === 0) {
+    console.log('n is zero')
+} else if( n % 2 === 0) {
+    console.log(n +' is Even Number')
+} else {
+    console.log(n +' is Odd Number')
+}

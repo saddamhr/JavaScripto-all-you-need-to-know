f6373b661f2b04b3f7928caa12f3c8ef0493cd6e
saddamhr <saddambubt65@gmail.com>
2020-04-06 13:44:53 +0600

    tarnary operator

---
+++ 3-chapter-three [handle-conditions-and-logic]/6-ternary-operator.js
@@ -0,0 +1,14 @@
+var n = 11
+var str = ''
+
+if (n % 2 === 0) {
+    str = 'EVEN'
+} else {
+    str = 'ODD'
+}
+
+console.log(str)
+
+// condition ? true : false
+var result = n % 2 === 0 ? 'EVEN' : 'ODD'
+console.log(result)

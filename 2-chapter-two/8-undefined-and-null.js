5a8fa0944b19db3f9e2bcd263ceda1bff67de214
saddamhr <saddambubt65@gmail.com>
2020-04-06 10:00:05 +0600

    undefined and null

---
+++ 2-chapter-two/8-undefined-and-null.js
@@ -0,0 +1,7 @@
+// Undefined: A variable without value always store undefined.
+// Null: A variable with unknown value can store null.
+var abc
+var xyz = null
+
+console.log(null)
+console.log(abc)

2c9f1eab5c37365511f72c629367ad3902ba96ec
saddamhr <saddambubt65@gmail.com>
2020-04-06 11:47:40 +0600

    date in JS

---
+++ 2-chapter-two/15-date.js
@@ -0,0 +1,12 @@
+// Date in JS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+var date = new Date()
+console.log(date.toDateString())
+console.log(date.toTimeString())
+console.log(date.toLocaleString())
+
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDate())
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())

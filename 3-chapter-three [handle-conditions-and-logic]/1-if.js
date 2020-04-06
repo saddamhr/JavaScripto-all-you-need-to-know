c3e6dd8ec799eb7800dd4c97b1fb470352ca896d
saddamhr <saddambubt65@gmail.com>
2020-04-06 12:37:40 +0600

    if in JS

---
+++ 3-chapter-three [handle-conditions-and-logic]/1-if.js
@@ -0,0 +1,20 @@
+var a = 100
+var b = 20
+
+if (a > b) {
+    console.log('A is grater than B')
+}
+
+if (a < b) {
+    console.log('B is grater than A')
+}
+
+var n = 50
+
+if( n % 2 === 0) {
+    console.log(n +' is Even Number')
+}
+
+if( n % 2 === 1) {
+    console.log(n +' is Odd Number')
+}

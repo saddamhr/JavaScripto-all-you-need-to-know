55ecd0c574f275c6020a82c89b7351465008a295
saddamhr <saddambubt65@gmail.com>
2020-04-06 10:23:51 +0600

    truthy and falsey value

---
+++ 2-chapter-two/truthy-and-falsey-values.js
@@ -0,0 +1,27 @@
+/**
+ * Truthy values in JavaScript
+ * '0'
+ * ' '
+ * let name = []; empty array
+ * let name = {}; empty object
+ * true
+ */
+
+ /**
+ * false — boolean false
+ * 0 — number zero
+ * “” — empty string
+ * null
+ * undefined
+ * NaN — Not A Number
+ */
+
+// const age = 0;
+// const name = '0';
+let name = null;
+
+if(name) {
+    console.log('Condition is true');
+} else {
+console.log('Condition is false');
+}

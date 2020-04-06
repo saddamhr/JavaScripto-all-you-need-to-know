dc147ac1f0b9d3021066db81b55310779f64caae
saddamhr <saddambubt65@gmail.com>
2020-04-06 23:28:10 +0600

    escape notation

---
+++ 05-chapter-five [string-and-it's-uses]/2-escape-notation.js
@@ -0,0 +1,8 @@
+// Escape Notation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+var str = 'This is a \'String\''
+console.log(str)
+
+var str2 = 'This is a \nString'
+var str3 = 'This is a \tString'
+var str4 = 'This is a \\tString'

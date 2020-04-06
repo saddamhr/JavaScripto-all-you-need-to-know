7f2cc4488bfa6810ae61ab526b71ca88a15073c5
saddamhr <saddambubt65@gmail.com>
2020-04-06 11:21:11 +0600

    operators in JS

---
+++ 2-chapter-two/12-operator.js
@@ -0,0 +1,66 @@
+// OPERATORS IN JS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
+
+// Arithmetic Operator: + - * / % ++ --
+
+var a = 10
+var b = 3
+// console.log(a % b)
+
+// Increment/ Decrement: Pre-Increment/ Decrement, Post-Increment/ Decrement
+// console.log(++a)
+// console.log(a++)
+// console.log(--a)
+// console.log(a--)
+// console.log(a)
+
+// Assignment Operator
+// var a = 10
+// var b = 20
+
+a += b //a = a + b
+// console.log(a)
+
+a -= b //a = a - b
+// console.log(a)
+
+a *= b
+// console.log(a)
+
+a /= b
+// console.log(a)
+
+a %= b
+// console.log(a)
+
+// Comparison Operator
+
+var a = 10
+var b = 20
+
+console.log(a == b) // false
+console.log(a != b) // true
+
+console.log(a > b) // false
+console.log(a < b) // true
+
+console.log(a >= b) // false
+console.log(a <= b) // true
+
+var c = '50'
+var d = 50
+
+console.log(c == d) // true
+console.log(c === d) // false
+console.log(c !== d) // true
+console.log(c != d) // false
+
+// Logical Operators: && || !
+// Bitwise Operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators 
+// AND: &
+// OR: |
+// XOR: ^
+// NOT: ~
+// Left Shift: <<
+// Right Shift: >>
+
+console.log(typeof '10') // typeof operator

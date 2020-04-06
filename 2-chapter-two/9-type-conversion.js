f036a602f9c5b63fef8a9ae115c860dcfedfdb20
saddamhr <saddambubt65@gmail.com>
2020-04-06 10:22:55 +0600

    type conversion

---
+++ 2-chapter-two/9-type-conversion.js
@@ -0,0 +1,28 @@
+var str = '1000'
+var n = 10
+
+console.log(Number(str))
+console.log(Number.parseInt(str))
+console.log(n.toString())
+
+console.log(Number(Infinity))
+console.log(String(Infinity))
+console.log(Boolean(Infinity))
+
+console.log(Boolean(''))
+console.log(Boolean('Saddam'))
+
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+
+console.log(Boolean(0))
+console.log(Boolean(451))
+
+console.log(true)
+var x = true
+console.log(x.toString())
+
+// Constructor in JS
+// String
+// Number 
+// Boolean

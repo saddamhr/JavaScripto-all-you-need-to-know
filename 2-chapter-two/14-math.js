d71f7ac47af17728a4bccf58b93587cd3981d33e
saddamhr <saddambubt65@gmail.com>
2020-04-06 11:36:37 +0600

    math in JS

---
+++ 2-chapter-two/14-math.js
@@ -0,0 +1,14 @@
+// Math in JS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+console.log(Math.E)
+console.log(Math.PI)
+var n = 4.589
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+console.log(Math.max(400, 500, 600))
+console.log(Math.min(400, 500, 200))
+console.log(Math.pow(2, 3))
+console.log(Math.sqrt(9))
+console.log(Math.round(Math.random() * 50 + 1))

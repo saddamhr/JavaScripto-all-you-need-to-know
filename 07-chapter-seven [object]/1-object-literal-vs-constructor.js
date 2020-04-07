d2624f795cd8b172c8e5e2856b72e0cd3e757f5c
saddamhr <saddambubt65@gmail.com>
2020-04-07 23:19:31 +0600

    object literal vs constructor

---
+++ 07-chapter-seven [object]/1-object-literal-vs-constructor.js
@@ -0,0 +1,21 @@
+// Object Literal
+var objOne = {}
+objOne.x = 10
+console.log(objOne)
+
+var point = {
+    x : 10,
+    y : 20
+}
+point.z = 30
+console.log(point)
+
+// Object Constructor
+var objTwo = Object()
+objTwo.a = 10
+console.log(objTwo)
+
+var objThree = new Object()
+objThree.b = 20
+console.log(objThree)
+

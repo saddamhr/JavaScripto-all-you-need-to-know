e4a562f56fa797eebb45dcd650393400b6ec23a9
saddamhr <saddambubt65@gmail.com>
2020-04-07 11:32:32 +0600

    array declaration

---
+++ 06-chapter-six [array-and-it's-uses]/2-array-declaration.js
@@ -0,0 +1,13 @@
+// Array Literal
+var arr = [1, 2, 3, 4, 5]
+arr[2] = 22
+arr[5] = 6
+arr[10] = 11
+console.log(arr)
+console.log(arr[0])
+console.log(arr[4])
+console.log(arr.length)
+
+// Array Constructor
+var arrTwo = Array(1, 2, 3)
+console.log(arrTwo)

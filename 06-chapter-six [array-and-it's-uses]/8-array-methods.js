f3ce74d84cfab0a8f95def1573e701596278e9ad
saddamhr <saddambubt65@gmail.com>
2020-04-07 14:09:30 +0600

    array methods

---
+++ 06-chapter-six [array-and-it's-uses]/8-array-methods.js
@@ -0,0 +1,21 @@
+// var arrOne = [4, 5, 9, 6]
+// // var arrTwo = [5, 10, 45, 23]
+
+// // console.log(arr.join(' | '))
+
+// var newArr = arrOne.concat(arrTwo)
+// // arr.fill(0)
+// // console.log(newArr)
+
+
+// console.log(Array.isArray(arrOne))
+
+// var arrTwo = Array.from(arrOne)
+// console.log(arrTwo)
+
+var a = [1, 2]
+var b = Array.from(a)
+
+b[0] = 5
+console.log(a)
+console.log(b)

cfd5e969c9d2b7bc08d4b6022f847afb36805179
saddamhr <saddambubt65@gmail.com>
2020-04-07 23:33:16 +0600

    accessing object

---
+++ 07-chapter-seven [object]/2-accessing-object.js
@@ -0,0 +1,17 @@
+var point = {
+    x : 10,
+    y : 20,
+    z : 15
+}
+// Dot Notation
+console.log(point.x)
+console.log(point.y)
+console.log(point.z)
+console.log(point.x + point.y + point.z)
+
+// Array Notation
+console.log(point['x'])
+console.log(point['z'])
+
+var show = 'x'
+console.log(point[show])

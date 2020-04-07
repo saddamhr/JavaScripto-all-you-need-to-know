e497090e4db11fc1b79cd4970519824d81696cb1
saddamhr <saddambubt65@gmail.com>
2020-04-07 13:48:15 +0600

    multidimension array

---
+++ 06-chapter-six [array-and-it's-uses]/6-multidimension-array.js
@@ -0,0 +1,11 @@
+var arr = [
+    [78, 71, 80, 90],
+    [80, 75, 90, 86],
+    [71, 75, 79, 80]
+]
+
+for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j])
+    }
+}

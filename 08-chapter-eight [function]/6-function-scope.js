845238c684d96964b9544dbc6048de74b9c7e283
saddamhr <saddambubt65@gmail.com>
2020-04-08 19:30:54 +0600

    function scope

---
+++ 08-chapter-eight [function]/6-function-scope.js
@@ -0,0 +1,36 @@
+var a = 'abc'
+
+if(true) {
+    if(true) {
+        var b = 'I am B'
+    }
+}
+console.log(b)
+
+function x() {
+    var a = 20
+    function y() {
+        var a = 10
+        console.log(a)
+    }
+    console.log(a)
+    y()
+}
+x()
+
+function testFunction(n) {
+    function a() {
+        return n % 3 === 0
+    }
+    function b() {
+        return n% 5 === 0
+    }
+
+    if(a() && b()) {
+        console.log(n + ' is divisible by both 3 and 5')
+    } else {
+        console.log('Not a valid number')
+    }
+}
+
+testFunction(15)

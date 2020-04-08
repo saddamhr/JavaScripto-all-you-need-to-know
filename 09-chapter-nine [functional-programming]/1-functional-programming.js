2ff382c95f02aa5fc62410cc89dc208e311a4f37
saddamhr <saddambubt65@gmail.com>
2020-04-08 22:48:00 +0600

    started new chapter: functional programming

---
+++ 09-chapter-nine [functional-programming]/1-functional-programming.js
@@ -0,0 +1,6 @@
+// Functional programming: Functional programming is a programming paradigm- a style of building the structure and elements of computer programs- theat treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data.
+
+// There main terms
+// 1. Pure Functions
+// 2. Higher order Functions
+// 3. Higher order Function

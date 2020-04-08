da3d2829e8446356fa3b85a806856d854347a13f
saddamhr <saddambubt65@gmail.com>
2020-04-08 09:54:18 +0600

    function

---
+++ 08-chapter-eight [function]/1-function.js
@@ -0,0 +1,22 @@
+function functionName () {
+    console.log('I am a function')
+}
+
+function add() {
+    var a = 10
+    var b = 20
+    console.log(a + b)
+}
+
+function sub() {
+    var a = 50
+    var b = 20
+    console.log(a - b)
+}
+
+functionName()
+// for(var i = 0; i < 10; i++) {
+//     functionName()
+// }
+add()
+sub()

d501f50413712eeb79557e689c6d80ac0e751b93
saddamhr <saddambubt65@gmail.com>
2020-04-10 22:01:54 +0600

    recursive function

---
+++ 09-chapter-nine [functional-programming]/14-recursive-function.js
@@ -0,0 +1,36 @@
+function sayHi(n) {
+    if (n === 0) {
+        return
+    }
+    console.log('Hi, I\'m calling')
+    sayHi(n - 1)
+}
+
+sayHi(10)
+
+function sum(n) {
+    if (n === 1) {
+        return 1
+    }
+    return n + sum(n - 1)
+}
+
+console.log(sum(5))
+
+function fact(n) {
+    if (n === 1) {
+        return 1
+    }
+    return n * fact(n - 1)
+}
+console.log(fact(4))
+
+var arr = [1, 2, 3, 4, 5]
+function sumOfArray(arr, lastIndex) {
+    if (lastIndex < 0) {
+        return 0
+    }
+    return arr[lastIndex] + sumOfArray(arr, lastIndex - 1)
+}
+
+console.log(sumOfArray(arr, arr.length - 1))

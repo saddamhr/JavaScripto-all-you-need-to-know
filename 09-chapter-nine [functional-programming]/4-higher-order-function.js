7e84cb998e81e0ef6920e39be13d0f4b02adc465
saddamhr <saddambubt65@gmail.com>
2020-04-10 09:46:56 +0600

    higer order function

---
+++ 09-chapter-nine [functional-programming]/4-higher-order-function.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b
+}
+
+function manipulate(a, b, func) {
+    var c = a + b
+    var d = a - b
+    // function multiply() {
+    //     var m = func(a, b)
+    //     return c * d * m
+    // }
+
+    return function() {
+        var m = func(a, b)
+        return c * d * m
+    }
+}
+
+var multiply = manipulate(3, 4, add)
+console.log(multiply())

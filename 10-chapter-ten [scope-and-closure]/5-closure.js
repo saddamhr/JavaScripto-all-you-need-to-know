5b943654f3ca22f0444cbde03e4d6b5e43333c7b
saddamhr <saddambubt65@gmail.com>
2020-04-11 13:11:05 +0600

    closure

---
+++ 10-chapter-ten [scope-and-closure]/5-closure.js
@@ -0,0 +1,23 @@
+// Closure: Closure is when a function is able to remember and access it's lexical scope even when that function executing outside it's lexical scope.
+
+// Able to remember and access it's lexical scope.
+// function test() {
+//     var msg = 'I\'m learning lexical scope and closure'
+    
+//     function sayMsg() {
+//         console.log(msg)
+//     }
+//     sayMsg()
+// }
+
+// when that function executing outside it's lexical scope.
+function test() {
+    var msg = 'I\'m learning lexical scope and closure'
+    
+    return function() {
+        console.log(msg)
+    }
+}
+
+var sayMsg = test()
+sayMsg()

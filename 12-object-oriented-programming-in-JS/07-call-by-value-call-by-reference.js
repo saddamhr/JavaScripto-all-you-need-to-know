d887c033469ab9c59a6ef65c6a55bcdf826e0707
saddamhr <saddambubt65@gmail.com>
2020-04-12 14:10:13 +0600

    call by value call by reference

---
+++ 12-object-oriented-programming-in-JS/07-call-by-value-call-by-reference.js
@@ -0,0 +1,23 @@
+var n = 10
+
+function change(n) {
+    n = n + 100
+    console.log(n)
+}
+
+change(n)
+console.log(n)
+
+var obj = {
+    a: 10,
+    b: 20
+}
+
+function changeMe() {
+    obj.a = obj.a + 100
+    obj.b = obj.b + 100
+    console.log(obj)
+}
+
+changeMe(obj)
+console.log(obj)

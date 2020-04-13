2a9348f63ed175f00e6c46d2201a4a8e695da479
saddamhr <saddambubt65@gmail.com>
2020-04-13 10:11:22 +0600

    abstraction

---
+++ 12-object-oriented-programming-in-JS/08-abstraction.js
@@ -0,0 +1,22 @@
+var Rectangle = function (width, height) {
+    this.width = width
+    this.height = height
+    var position = {
+        x: 56,
+        y: -100
+    }
+
+    var printProperties = function () {
+        console.log('My width is ' + this.width)
+        console.log('My height is ' + this.height)
+    }.bind(this)
+
+    this.draw = function () {
+        console.log('I am a rectangle')
+        printProperties()
+        console.log('Position: X = ' + position.x + ' Y = ' + position.y)
+    }
+}
+
+var rectOne = new Rectangle(45, 30)
+rectOne.draw()

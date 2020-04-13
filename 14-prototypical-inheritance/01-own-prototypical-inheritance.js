4ee07ca313caf11258e4c204acd065f527f96197
saddamhr <saddambubt65@gmail.com>
2020-04-13 14:06:49 +0600

    inheritance

--- 14-prototypical-inheritance/01-own-prototypical-inheritance.js
+++ 14-prototypical-inheritance/01-own-prototypical-inheritance.js
@@ -1,26 +1,50 @@
-function Shape() {
-
+function extend(Parent, Child) {
+    Child.prototype = Object.create(Parent.prototype)
+    Child.prototype.constructor = Child
 }
-Shape.prototype = {
-    common: function () {
-        console.log('I am common method')
-    }
+function Shape(color) {
+    this.color = color
+}
+Shape.prototype.common = function () {
+    console.log('I am common method')
 }
 
 function Square(width) {
+    Shape.call(this, color)
     this.width = width
 }
-Square.prototype = Object.create(Shape.prototype)
+
+extend(Shape, Square)
 
 Square.prototype.draw = function () {
     console.log('Drawing')
 }
 
-var shape = new Shape()
-var sqr = new Square(45)
-
-function Circle() {
-
+Square.prototype.common = function() {
+    console.log('I am calling from square and I have overridden')
 }
 
-Circle.prototype = Object.create(Shape.prototype)
+// var shape = new Shape()
+var sqr = new Square(45, 'green')
+
+function Circle(radius, color) {
+    Shape.call(this.color)
+    this.radius = radius
+}
+
+extend(Shape, Circle)
+
+Circle.prototype.common = function() {
+    // Shape.prototype.common.call(this)
+    console.log('I am calling from circle and I have overridden')
+}
+
+var c = new Circle(4, 'Black')
+
+var s = new Shape('Pirple')
+
+var shapes = [s, c, sqr]
+
+for(var i of shapes) {
+    i.common()
+}

3ac2747a52529c7e998d7ecd42b8ba08b8d94590
saddamhr <saddambubt65@gmail.com>
2020-04-14 18:52:25 +0600

    static method

---
+++ 15-ES6 [ECMAScript]/20-static-methods.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name, email) {
+        this.name = name
+        this.email = email
+    }
+
+    print() {
+        console.log(this.name, this.email)
+    }
+
+    static create(str) {
+        let person = JSON.parse(str)
+        return new Person(person.name, person.email)
+    }
+}
+
+let str = '{"name": "Saddam Hossain", "email": "saddambubt65@gmail.com"'
+let p1 = Person.create(str)
+console.log(p1)
+console.log(p1 instanceof Person)
+p1.print()

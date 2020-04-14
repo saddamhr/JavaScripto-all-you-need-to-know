a33b4f9a52853ba9295a23c572a5ab4a941cc5e3
saddamhr <saddambubt65@gmail.com>
2020-04-14 11:17:50 +0600

    iterator

--- 15-ES6 [ECMAScript]/11-iterator.js
+++ 15-ES6 [ECMAScript]/11-iterator.js
@@ -1,21 +1,27 @@
 const arr = [1, 2, 3]
 
-function createIterator(collection) {
-    let i = 0
-    return {
-        next() {
-            return {
-                done: i >= collection.length,
-                value: collection[i++]
-            }
-        }
-    }
-}
+// function createIterator(collection) {
+//     let i = 0
+//     return {
+//         next() {
+//             return {
+//                 done: i >= collection.length,
+//                 value: collection[i++]
+//             }
+//         }
+//     }
+// }
 
-let iterate = createIterator(arr)
+let iterate = arr[Symbol.iterator]()
 console.log(iterate.next())
 console.log(iterate.next())
 console.log(iterate.next())
 console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
+
+let str = 'TEXT'
+let iterateText = str[Symbol.iterator]()
+
+console.log(iterateText.next())
+console.log(iterateText.next())
+console.log(iterateText.next())
+console.log(iterateText.next())

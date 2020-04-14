3d55ef3cf2cd950d22c2f7698f7e782634ec2e03
saddamhr <saddambubt65@gmail.com>
2020-04-14 20:07:36 +0600

    method overriding

---
+++ 15-ES6 [ECMAScript]/24-method-overriding.js
@@ -0,0 +1,28 @@
+class Shape {
+    constructor(color) {
+        this.color = color
+    }
+    draw() {
+        console.log('Drawing...')
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(color, width, height) {
+        super(color)
+        this.width = width
+        this.height = height
+    }
+
+    draw() {
+        console.log('Drawing a rectangle...')
+    }
+
+    calculate() {
+        return this.width * this.height
+    }
+}
+
+let r = new Rectangle('green',4, 5)
+console.log(r)
+r.draw()

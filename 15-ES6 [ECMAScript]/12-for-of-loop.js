acddcb17d2fec68f883eb6fff2c5b9231f710d78
saddamhr <saddambubt65@gmail.com>
2020-04-14 11:25:48 +0600

    for of loop

---
+++ 15-ES6 [ECMAScript]/12-for-of-loop.js
@@ -0,0 +1,19 @@
+// For of Loop
+const arr = [1, 2, 3]
+
+for(let v of arr) {
+    console.log(v)
+}
+
+for(let v of 'Saddam') {
+    console.log(v)
+}
+
+let obj = {
+    a: 10,
+    b: 20
+}
+
+for(let v of obj) {
+    console.log(v)
+}

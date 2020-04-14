c06e8fcfb82a1a40726be7a806fab3e0330963a8
saddamhr <saddambubt65@gmail.com>
2020-04-14 23:25:37 +0600

    finally block

--- 16-error-handling/04-throw-an-error.js
+++ 16-error-handling/04-throw-an-error.js
@@ -1,9 +1,11 @@
-try{
+try {
     console.log('I am Line 1')
     throw new Error('I am your error')
     console.log('I am Line 2')
     console.log('I am Line 3')
     console.log('I am Line 4')
-} catch(e) {
+} catch (e) {
     console.log(e.message)
+} finally {
+    console.log('I am finally block')
 }

bcf717d94a4aa33ee93d0ed989209b2c26bf6a1f
saddamhr <saddambubt65@gmail.com>
2020-04-16 20:12:18 +0600

    event delegation in DOM

--- 18-document-object-model-with-ajax/06-event-in-DOM.js
+++ 18-document-object-model-with-ajax/06-event-in-DOM.js
@@ -6,18 +6,31 @@ let btn = document.getElementById('btn')
 // }
 
 let list = document.getElementById('list')
-btn.addEventListener('click', function(e){
+btn.addEventListener('click', function (e) {
     let item = list.lastElementChild.cloneNode(true)
     item.innerHTML = 'Newly create item'
     list.appendChild(item)
+});
+
+// Event Delegation problem
+// [...list.children].forEach(li => {
+//     li.onclick = function (e) {
+//         e.target.remove()
+//     }
+// })
+
+list.addEventListener('click', function(e){
+   if(this.contains(e.target)){
+       e.target.remove()
+   } 
 })
 
 let box = document.getElementById('box')
-box.addEventListener('mousemove', function(e){
+box.addEventListener('mousemove', function (e) {
     // console.log(e)
     document.getElementById('x-value').innerHTML = e.offsetX
     document.getElementById('y-value').innerHTML = e.offsetY
-    if(e.offsetX === 50 && e.offsetY === 0) {
+    if (e.offsetX === 50 && e.offsetY === 0) {
         alert('50 0')
     }
 })

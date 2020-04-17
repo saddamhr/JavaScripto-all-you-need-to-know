4ca3f20580b65154477e9eb404b00473aae7eada
saddamhr <saddambubt65@gmail.com>
2020-04-17 08:21:11 +0600

    list item and input element event hadnling

--- 18-document-object-model-with-ajax/07-input-event-and-event-handling.js
+++ 18-document-object-model-with-ajax/07-input-event-and-event-handling.js
@@ -8,3 +8,55 @@ name.addEventListener('keypress', function (event) {
         event.target.value = ''
     }
 })
+
+let skills = document.getElementsByName('skills');
+let result = document.getElementById('result')
+
+let checkedSkills = [];
+
+[...skills].forEach(skill => {
+    skill.addEventListener('change', function (event) {
+        if (event.target.checked) {
+            checkedSkills.push(event.target.value)
+            outputSkills(result, checkedSkills)
+            // console.log(checkedSkills)
+        } else {
+            let ind = checkedSkills.indexOf(event.target.value)
+            checkedSkills.splice(ind, 1)
+            outputSkills(result, checkedSkills)
+            // console.log(checkedSkills)
+        }
+    })
+})
+
+function outputSkills(parent, skills) {
+    let result = ''
+    skills.forEach((skill, index) => {
+        result += `(${index + 1}) ${skill}`
+    })
+    parent.innerHTML = result
+}
+
+let list = document.getElementById('list')
+list.addEventListener('dblclick', function(event){
+    if(this.contains(event.target)) {
+        let innerText = event.target.innerText
+        event.target.innerHTML = ''
+        let inputBox = createInputBox(innerText)
+        event.target.appendChild(inputBox)
+
+        inputBox.addEventListener('keypress', function(e){
+            if(e.key === 'Enter') {
+                event.target.innerHTML = e.target.value
+            }
+        })
+    }
+})
+
+function createInputBox(value) {
+    let inp = document.createElement('input')
+    inp.type = 'text'
+    inp.className = 'form-control'
+    inp.value = value
+    return inp
+}

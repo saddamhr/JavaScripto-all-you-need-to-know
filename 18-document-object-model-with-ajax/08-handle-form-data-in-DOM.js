97d48a622ceebe3d1e8d5323a4055e2de9bf23d0
saddamhr <saddambubt65@gmail.com>
2020-04-17 09:43:40 +0600

    form validation in DOM

--- 18-document-object-model-with-ajax/08-handle-form-data-in-DOM.js
+++ 18-document-object-model-with-ajax/08-handle-form-data-in-DOM.js
@@ -1,16 +1,32 @@
 let form = document.getElementById('form')
 
-form.addEventListener('submit', function(e){
-   e.preventDefault()
-//    console.log(this.elements[0])
-//    console.log(this['email'])
-    let formData = {}
-   ;[...this.elements].forEach(el =>{
-    //    console.log(el.type, el.name, el.value)
-    if(el.type !== 'submit') {
-        formData[el.name] = el.value
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    //    console.log(this.elements[0])
+    //    console.log(this['email'])
+    let isValid = false
+    let formData = {};
+    [...this.elements].forEach(el => {
+        //    console.log(el.type, el.name, el.value)
+        if (el.type !== 'submit') {
+            isValid = validator(el)
+            if (isValid) {
+                formData[el.name] = el.value
+            } else {
+                el.classList.add('is-invalid')
+                alert(`${el.name} is required`)
+            }
+        }
+    })
+    if (isValid) {
+        console.log(formData)
+        this.reset()
     }
-   })
-   console.log(formData)
-   this.reset()
 })
+
+function validator(el) {
+    if(!el.value){
+        return false
+    }
+    return true
+}
